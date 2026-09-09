8255b9aea7fe12812abacd28047e2cd839ba0fe9
Freedom Chukwudi <editorfreedom01@gmail.com>
2026-09-10 00:24:18 +0100

    feat: set initial state of showPortal to false in App component

--- medical-website/src/App.jsx
+++ medical-website/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/public/Home';
 import Portal from './pages/patient/Portal';
 
 function App() {
-  const [showPortal, setShowPortal] = useState(true);
+  const [showPortal, setShowPortal] = useState(false);
 
   return showPortal ? <Portal onBack={() => setShowPortal(false)} /> : <Home />;
 }
